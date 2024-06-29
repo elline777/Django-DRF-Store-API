cf1adbf8d2d9d83041e5f6920a2dfb495a01ff90
Natalia <47737431+elline777@users.noreply.github.com>
2024-06-29 22:39:14 +0500

    Implement authentication and authorization

--- api/models.py
+++ api/models.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
 # Create your models here.
 class ApiUser(AbstractUser):
     ROLE_CHOICES = (
-        ('administrator', 'Administrator'),
-        ('customer', 'Customer'),
-        ('seller', 'Seller')
+        ('customer', 'Клиент'),
+        ('seller', 'Поставщик')
     )
 
     role = models.CharField(max_length=50, choices=ROLE_CHOICES)
@@ -17,6 +17,9 @@ class Store(models.Model):
     name = models.CharField(max_length=128)
     address = models.TextField()
 
+    def __str__(self):
+        return f'Склад {self.id}: {self.name}'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
@@ -24,7 +27,13 @@ class Product(models.Model):
     store = models.ForeignKey(Store, related_name = 'products',
                               on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Товар {self.id}: {self.name}. Цена: {self.price} '
+
 
 class Order(models.Model):
     product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)
     user = models.ForeignKey(ApiUser, related_name='orders', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username}; {self.product.name}; {self.product.store.name}'
