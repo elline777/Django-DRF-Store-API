d8d6b16b1a54f3a265852c0286d52b5b6d49e8ae
Natalia <47737431+elline777@users.noreply.github.com>
2024-09-26 22:00:04 +0500

    Implement supply and order functionality

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from rest_framework.routers import DefaultRouter
 
 from api.views import (UserModelViewSet, StoreModelViewSet,
-                       ProductModelViewSet, OrderModelViewSet)
+                       ProductModelViewSet, OrderModelViewSet,
+                       SupplyModelViewSet)
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
-router.register('stores', StoreModelViewSet)
-router.register('products', ProductModelViewSet)
-router.register('orders', OrderModelViewSet)
+router.register('users', UserModelViewSet, basename='users')
+router.register('stores', StoreModelViewSet, basename='stores')
+router.register('products', ProductModelViewSet, basename='products')
+router.register('supplies', SupplyModelViewSet, basename='supplies')
+router.register('orders', OrderModelViewSet, basename='orders')
 
 urlpatterns = [
 
--- api/views.py
+++ api/views.py
@@ -3,11 +3,12 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from api.permissions import IsSeller, IsCustomer
+from api.permissions import IsSeller, IsCustomer, IsOwnerOrReadOnly
 
-from api.models import ApiUser, Store, Product, Order
+from api.models import ApiUser, Store, Product, Order, Supply
 from api.serializers import UserSerializer, StoreSerializer, ProductSerializer, \
-    OrderSerializer, OrderWriteSerializer
+    OrderSerializer, SupplySerializer, SupplyWriteSerializer, \
+    OrderWriteSerializer
 
 
 # Create your views here.
@@ -27,37 +28,66 @@ class StoreModelViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows stores to be viewed or edited.
     """
-    permission_classes = [IsAuthenticatedOrReadOnly, IsSeller]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsSeller, IsOwnerOrReadOnly]
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
 
-    @action(detail=True)
-    def products(self, request, pk=None):
-        store = get_object_or_404(Store.objects.all(), id=pk)
-        store_products = store.products.filter(orders__isnull=True)
-        return Response(
-            ProductSerializer(store_products, many=True).data
-        )
-
+    # @action(default=True)
+    # def products(selfself, request, pk=None):
+    #     store = get_object_or_404(Store.objects.all(), id=pk)
+    #     available_products = Product.objects.filter(availability__store=store, availability__quantity__gt=0)
+    #     return Response(
+    #         ProductSerializer(available_products, many=True).data
+    #     )
 
 class ProductModelViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows products to be viewed or edited.
     """
-    permission_classes = [IsAuthenticatedOrReadOnly, IsSeller]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsSeller, IsOwnerOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class SupplyModelViewSet(viewsets.ModelViewSet):
+    """
+       API endpoint that allows supplies to be viewed or edited.
+       """
+    permission_classes = [IsAuthenticatedOrReadOnly, IsSeller, IsOwnerOrReadOnly]
+    queryset = Supply.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return SupplyWriteSerializer
+
+        return SupplySerializer
 
 
 class OrderModelViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows orders to be viewed or edited.
     """
-    permission_classes = [IsAuthenticatedOrReadOnly, IsCustomer]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsCustomer, IsOwnerOrReadOnly]
     queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
     def get_serializer_class(self):
         if self.action in ["create", "update", "partial_update", "destroy"]:
             return OrderWriteSerializer
 
         return OrderSerializer
+
+    # def get_permissions(self):
+    #     if self.action == 'list':
+    #         permission_classes = [IsAuthenticated]
+    #     else:
+    #         permission_classes = [IsAdminUser]
+    #     return [permission() for permission in permission_classes]
+    #
+#
+# class AvailabilityModelViewSet(viewsets.ReadOnlyModelViewSet):
+#     """
+#     A simple ViewSet for viewing accounts.
+#     """
+#     queryset = Availability.objects.all()
+#     serializer_class = AvailabilitySerializer
